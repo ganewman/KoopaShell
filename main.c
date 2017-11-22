0818e9b6d7c2f760aac1bab6eaad60fbd453f91b
gnewman00 <gnewman@stuy.edu>
2017-11-22 11:47:37 -0500

    push working code

--- main.c
+++ main.c
@@ -4,29 +4,34 @@
 #include <string.h>
 
 int countArgs(char *);
-char * parseCommand(char *);
+char ** parseCommand(char *);
 
 int main(){
   while(1){
     char s[256];
     printf("Please type a command:\n"); // FIXME: print command prompt instead
     fgets(s, sizeof(s), stdin);
-    printf("%d\n", countArgs(s));
-
+    char ** args = parseCommand(s);
+    /*   int i;
+    for (i = 0; args[i]; i++){
+      printf("%s\n", args[i]);
+      }*/
   }
   return 0;
 }
 
 
-char * parseCommand(char * command){
+char ** parseCommand(char * command){
   int i = 0;
   char space[] = " ";
   int num_args = countArgs(command);
-  char ** args = calloc(num_args, sizeof(char *));
-  while(*command){
+  char ** args = calloc(num_args + 1, sizeof(char *));
+  //  printf("%s\n", command);
+  while(command){
     args[i] = strsep(&command, space);
     i++;
-  }	       
+  }
+  args[num_args] = 0;
   return args;
 
 }
